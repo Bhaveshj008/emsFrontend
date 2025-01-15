0b711496e6a3633e4625523543193c87b1e21ce1
bhavesh008 <bhavesh002jadhav@gmail.com>
2025-01-15 15:09:50 +0530

    first commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getToken } from './auth';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://emsbackend-kappa.vercel.app/api/';
 
 const api = axios.create({
   baseURL: API_URL
